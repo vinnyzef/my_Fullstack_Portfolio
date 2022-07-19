9549a3321bcc86c2564d23861fa86991b301d3c3
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-07-19 12:22:23 -0500

    working as of now 719 starting on project diplay

--- frontend_react/src/Components/Navbar/Navbar.jsx
+++ frontend_react/src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.scss'
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion'
+
 import { images } from '../../Constants'
+
 const Navbar = () => {
+  const [toggle, settoggle] = useState(false)
   return (
     <nav className='app__navbar'>
         <div className='app__navbar-logo'>
-            <img src={images.logo} alt= 'logo'/>
+            {/* <img src={images.logo} alt= 'logo'/> */}
+            VINCENT ZEFRAN
         </div>
-        <ul className='app__navbar-logo'>
+        <ul className='app__navbar-links'>
             {['home','about','work','skills','contact'].map((item)=><li className='app__flex p-text' key={`link-${item}`}>
                 <div/>
             <a href={`#${item}`}>{item}</a>
             </li>)}
         </ul>
+        <div className='app__navbar-menu'>
+
+        <HiMenuAlt4 onClick={()=> settoggle(true)} />
+
+        {
+           toggle && (<motion.div
+           whileInView={{x: [300,0]}}
+           transition={{duration: 0.85, ease: 'easeOut'}}>
+
+          <HiX onClick={()=> settoggle(false)}/>
+          <ul>
+          {['home','about','work','skills','contact'].map((item)=> (<li key={item}>
+            <a href={`#${item}`}onClick={()=> settoggle(false)}>{item}</a>
+            </li>
+            ))}
+           </ul>
+
+           </motion.div> 
+          )}
+        </div>
     </nav>
   )
 }
--- frontend_react/src/Container/About/About.jsx
+++ frontend_react/src/Container/About/About.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import { urlFor, client } from '../../client'
 import './About.scss'
+import { images } from '../../Constants'
+import { motion } from 'framer-motion'
+import { AppWrap } from '../../wrapper'
+
 const About = () => {
+
+  const [abouts, setAbouts] = useState([])
+
+
+  useEffect(() => {
+   const query = '*[_type == "abouts"]';
+    client.fetch(query)
+    .then((data)=>  setAbouts(data))
+    .catch(console.error())
+  }, [])
+  
   return (
-    <div>about</div>
+    <>
+    <h2 className='head-text'>
+     a <span>fresh</span>  outlook on Development<br/>and <span>trustworthy</span> Business
+
+    </h2>
+    <div className='app__profiles'>
+      {abouts.map((about, index)=>(
+        <motion.div
+        whileInView={{opacity:1}} 
+        whileHover={{scale: 1.1}}
+        transition={{duration: 0.5, type:'tween'}}
+        className="app__profile-item"
+        key={about.title+index}>
+          <img src={urlFor(about.imgUrl)} alt={about.title}/>
+          <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+          <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+        </motion.div>
+      ))}
+    </div>
+    
+    
+    
+    </>
   )
 }
 
-export default About
+export default AppWrap(About, 'about')
